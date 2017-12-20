6bed4f5a196c3bf5e947dc311b5e30acc454ac5a
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-12-20 18:28:45 +0000

    Add Header and Home Section

--- resources/views/template-home-page.blade.php
+++ resources/views/template-home-page.blade.php
@@ -4,21 +4,33 @@
 
 @extends('layouts.app')
 
-<div class="home">
-  <div class="container">
+@section('content')
 
-    <div class="big-title">
-      <h1>Quality Doesn’t Have To Be <b>Expensive</b></h1>
-      <p>
-        Jakiro grew out of a love affair with the rising independent fashion, open attitude,<br>
-        unexpected pairings, and appreciation for all things<br>
-        ecclectic and unique.
-      </p>
-      <a href="#" class="btn btn-light">
-        Shop Now
-        <i class="la la-shopping-cart"></i>
-      </a>
+  {{-- # ############################ # -->
+  <!-- #                              # -->
+  <!-- #          Main Section        # -->
+  <!-- #                              # -->
+  <!-- # ############################ # --}}
+  <div class="main-section">
+    <div class="container">
+
+      <div class="big-title">
+        <h1>Quality Doesn’t Have To Be <b>Expensive</b></h1>
+        <p>
+          Jakiro grew out of a love affair with the rising independent fashion, open attitude,<br>
+          unexpected pairings, and appreciation for all things<br>
+          ecclectic and unique.
+        </p>
+        <a href="{!! App::wooCommerceUrl('shop') !!}" class="btn btn-light">
+          Shop Now
+          <i class="la la-shopping-cart"></i>
+        </a>
+      </div>
+
     </div>
-
   </div>
-</div>
+
+
+
+
+@endsection
