bfd5a5a349b5037e183f6b4b6792961b442ce11d
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-12-27 22:10:31 +0000

    Add 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -3,7 +3,19 @@
 @section('content')
   @include('layouts/page-breadcrumbs')
 
-  @if (!have_posts())
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'dailycomfort') }}
-  @endif
+  <div class="page-container">
+    @if (!have_posts())
+      <section>
+        <header>
+          <h1>404</h1>
+          <h2>Page Not Found!</h2>
+          <a href="#" class="btn btn-light">
+            Back to homepage
+            <i class="arrow"></i>
+          </a>
+        </header>
+      </section>
+    @endif
+  </div>
+
 @endsection
