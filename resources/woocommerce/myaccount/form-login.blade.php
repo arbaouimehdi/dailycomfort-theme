822671a2526580494f424536ac0e965f9e777ab7
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-02 20:58:12 +0000

    Add form login
    Add form login and notice

--- resources/woocommerce/myaccount/form-login.blade.php
+++ resources/woocommerce/myaccount/form-login.blade.php
@@ -1,16 +1,14 @@
-{{ wc_print_notices() }}
+<?php wc_print_notices(); ?>
 
-{{ do_action('woocommerce_before_customer_login_form') }}
 
 <div class="row justify-content-md-center">
   <div class="col col-lg-6">
-    <form method="post">
 
-      {{ do_action('woocommerce_login_form_start') }}
+    <form method="post">
 
       {{-- Username or Email --}}
       <div class="form-group">
-        <label for="username">Username or Email address</label>
+        <label for="username">Username or Email address <span class="required">*</span></label>
         <input type="text"
                class="form-control"
                name="username"
@@ -21,7 +19,7 @@
 
       {{-- # Password --}}
       <div class="form-group">
-        <label for="password">Password</label>
+        <label for="password">Password <span class="required">*</span></label>
         <input type="password"
                class="form-control"
                name="password"
@@ -30,26 +28,20 @@
       </div>
 
 
-      {{ do_action('woocommerce_login_form') }}
-
       {{-- # Login & Remember me --}}
       <div class="form-group form-actions">
-        <?php wp_nonce_field('woocommerce-login', 'woocommerce-login-nonce' ); ?>
-        <button type="submit" class="btn btn-dark" name="login">
-          Login
-          <i class="arrow"></i>
-        </button>
-
-       <span>
-          Remember me <input name="rememberme" type="checkbox" id="rememberme" value="forever" />
-       </span>
-
+        <?php wp_nonce_field('woocommerce-login'); ?>
+        <input type="submit" class="btn btn-dark" name="login"
+               value="<?php esc_attr_e('Login', 'woocommerce'); ?>"/>
+        <input type="hidden" name="redirect" value="<?php echo esc_url($redirect_url); ?>"/>
+        <span>
+          <input name="rememberme" type="checkbox" id="rememberme" value="forever"/>
+          Remember me
+        </span>
       </div>
 
       {{-- # Lost Password --}}
       <a href="{{ esc_url(wp_lostpassword_url()) }}">Lost your password?</a>
-
-      {{ do_action('woocommerce_login_form_end') }}
 
     </form>
   </div>
