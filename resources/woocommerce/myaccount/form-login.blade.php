e059ddabc9fab262d7ae753de3aa249227d026f6
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-02 22:01:44 +0000

    Add Register Form

--- resources/woocommerce/myaccount/form-login.blade.php
+++ resources/woocommerce/myaccount/form-login.blade.php
@@ -1,10 +1,16 @@
 <?php wc_print_notices(); ?>
 
-{{-- # Login --}}
+{{-- # My Account --}}
 <div class="row justify-content-md-center">
+
+  {{-- # Login --}}
   <div class="col col-lg-6">
 
+    <h2>Login</h2>
+
     <form method="post">
+
+      <?php do_action('woocommerce_login_form_start'); ?>
 
       {{-- Username or Email --}}
       <div class="form-group">
@@ -27,7 +33,7 @@
                placeholder="Enter your password">
       </div>
 
-      <?php do_action( 'woocommerce_login_form' ); ?>
+      <?php do_action('woocommerce_login_form'); ?>
 
       {{-- # Login & Remember me --}}
       <div class="form-group form-actions">
@@ -44,10 +50,56 @@
       {{-- # Lost Password --}}
       <a href="{{ esc_url(wp_lostpassword_url()) }}">Lost your password?</a>
 
-      <?php do_action( 'woocommerce_login_form_end' ); ?>
+      <?php do_action('woocommerce_login_form_end'); ?>
 
     </form>
   </div>
+
+  {{-- # Register --}}
+  @if (get_option( 'woocommerce_enable_myaccount_registration') === 'yes')
+    <div class="col col-lg-6">
+
+      <h2>Register</h2>
+
+      <form method="post">
+
+        <?php do_action('woocommerce_register_form_start'); ?>
+
+        {{-- # Email --}}
+        <div class="form-group">
+          <label for="reg_email">Email address <span class="required">*</span></label>
+          <input type="email"
+                 name="email"
+                 class="form-control"
+                 id="reg_email"
+                 value="<?php echo (!empty($_POST['email'])) ? esc_attr($_POST['email']) : ''; ?>"/>
+        </div>
+
+        {{-- # Password --}}
+        <div class="form-group">
+          <label for="reg_password">Password <span class="required">*</span></label>
+          <input type="password"
+                 name="password"
+                 class="form-control"
+                 id="reg_password"/>
+        </div>
+
+        {{-- # Register --}}
+        <?php do_action('woocommerce_register_form'); ?>
+
+
+        <div class="form-group form-actions">
+          <?php wp_nonce_field('woocommerce-register', 'woocommerce-register-nonce'); ?>
+          <input type="submit" class="btn btn-dark" name="register"
+                 value="<?php esc_attr_e('Register', 'woocommerce'); ?>"/>
+        </div>
+
+        <?php do_action('woocommerce_register_form_end'); ?>
+
+      </form>
+    </div>
+  @endif
+
 </div>
 
-
+<?php do_action('woocommerce_after_customer_login_form'); ?>
