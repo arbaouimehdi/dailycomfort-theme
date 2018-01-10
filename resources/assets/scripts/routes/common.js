db7665ec3abbc9dbb6a713f5cfdbc06d05115730
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-10 21:07:08 +0000

    Move addItemToCart and removeItemToCart to common.js

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,60 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    /**
+     *
+     * Remove item from cart list
+     *
+     */
+    $('.remove-item').click(function(){
+      let product_id = $(this).data().productId;
+      let list       = $(this).closest('li');
+      list.append('<div class="remove-item-preloader"></div>');
+
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: "/wp-admin/admin-ajax.php",
+        data: {action : 'removeItemFromCart','product_id': product_id},
+        success: function(data){
+
+          console.log(data);
+
+          list.remove();
+          $('.remove-item-preloader').remove();
+
+          // If no product is left on the list
+          if ($('.menu-sc-list ul li').length < 1) {
+            $('.menu-sc-list').html('<p>No products in the cart. No products in the cart.</p>');
+          }
+
+        },
+      });
+      return false;
+    });
+
+    /**
+     *
+     * Add item to cart list
+     *
+     */
+    $('.add-to-cart').click(function(){
+      let product_id = $(this).data().productId;
+      let product_quantity = $(this).data().productQuantity;
+
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: "/wp-admin/admin-ajax.php",
+        data: {action : 'addItemToCart', 'product_id': product_id, 'product_quantity': product_quantity},
+        success: function(data){
+          console.log(data);
+        },
+      });
+      return false;
+    })
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/assets/scripts/routes/woocommerce.js
+++ resources/assets/scripts/routes/woocommerce.js
@@ -23,36 +23,6 @@ export default {
 
     });
 
-    /**
-     *
-     * Remove an item list
-     *
-     */
-    $('.remove-item').click(function(){
-      let product_id = $(this).data().productId;
-      let list       = $(this).closest('li');
-      list.append('<div class="remove-item-preloader"></div>');
-
-      $.ajax({
-        type: 'POST',
-        dataType: 'json',
-        url: "/wp-admin/admin-ajax.php",
-        data: {action : 'remove_item_from_cart','product_id' : product_id},
-        success: function(){
-
-          list.remove();
-          $('.remove-item-preloader').remove();
-
-          // If no product is left on the list
-          if ($('.menu-sc-list ul li').length < 1) {
-            $('.menu-sc-list').html('<p>No products in the cart. No products in the cart.</p>');
-          }
-
-        },
-      });
-      return false;
-    });
-
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
